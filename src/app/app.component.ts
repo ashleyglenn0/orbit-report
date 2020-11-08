a48dbbe8e0d87ec4235db311edeff0c8cfb19fa4
Ashley Glenn <ashleyglenn@Ashleys-MBP-2.attlocal.net>
2020-11-08 12:00:12 -0600

    adding api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,28 @@ export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
   constructor() {
-    this.sourceList = [
-      new Satellite("SiriusXM","Communication","2009-03-21","LOW", true),
-      new Satellite("Cat Scanner","Imaging","2012-01-05","LOW", true),
-      new Satellite("Weber Grill","Space Debris","1996-03-25","HIGH", false),
-      new Satellite("GPS 938","Positioning","2001-11-01","HIGH", true),
-      new Satellite("ISS","Space Station","1998-11-20","LOW", true),
-    ];
+    this.sourceList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+  
+  window.fetch(satellitesUrl).then(function(response){
+    response.json().then(function(data){
+      let fetchedSatellites = data.satellites;
+      //Loop over satellites
+      for(let i = 0; i < satellitesUrl.length; i++){
+        
+      }
+      // Create new Satellite object using new Satellite
+       let newSatellite = { 
+
+       }
+
+
+      // add new Satellite object to sourceList using: this.sourceList.push(satellite);
+       
+    }.bind(this));
+  }.bind(this));
+  
+  
   }
 }
 
