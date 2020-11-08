98215f7df60f59b7ec8d8d896b6dee8d433d5176
Ashley Glenn <ashleyglenn@Ashleys-MBP-2.attlocal.net>
2020-11-08 12:16:39 -0600

    added new satellite object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,16 +19,12 @@ export class AppComponent {
       let fetchedSatellites = data.satellites;
       //Loop over satellites
       for(let i = 0; i < satellitesUrl.length; i++){
-        
+        // Create new Satellite object using new Satellite
+        new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+       // add new Satellite object to sourceList using: this.sourceList.push(satellite);
+      this.sourceList.push(Satellite);
       }
-      // Create new Satellite object using new Satellite
-       let newSatellite = { 
-
-       }
-
-
-      // add new Satellite object to sourceList using: this.sourceList.push(satellite);
-       
+      
     }.bind(this));
   }.bind(this));
   
