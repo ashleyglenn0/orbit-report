02910e5e344d85926a7b8214aad1bcb25795f59b
Ashley Glenn <ashleyglenn@Ashleys-MBP-2.attlocal.net>
2020-11-09 01:19:20 -0600

    updated search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
+  searchTerm: string;
+
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
 
     window.fetch(satellitesUrl).then(function (response) {
@@ -28,6 +32,7 @@ export class AppComponent {
             fetchedSatellites[i].operational);
           // add new Satellite object to sourceList using: this.sourceList.push(satellite);
           this.sourceList.push(satellite);
+          this.displayList = this.sourceList.slice(0);
         }
       }.bind(this));
     }.bind(this));
